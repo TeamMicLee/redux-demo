a81816504bc03c1368259bff82e94ae04a74aa6c
litao <TeamMicLee@foxmail.com>
2018-01-15 10:34:40 +0800

    增加fetch请求异步操作终止

--- redux/weather_redux/src/weather/actions.js
+++ redux/weather_redux/src/weather/actions.js
@@ -1,5 +1,7 @@
 import {FETCH_STARTED, FETCH_SUCCESS, FETCH_FAILURE} from './actionTypes.js';
 
+let nextSeqId = 0;
+
 export const fetchWeatherStarted = () => ({
     type: FETCH_STARTED
 });
@@ -17,8 +19,15 @@ export const fetchWeatherFailure = (error) => ({
 export const fetchWeather = (cityCode) => {
     return (dispatch) => {
         const apiUrl = `/data/cityinfo/${cityCode}.html`;
+        const seqId = ++ nextSeqId;
 
-        dispatch(fetchWeatherStarted);
+        const dispatchIfValid = (action) => {
+            if (seqId === nextSeqId) {
+                return dispatch(action);
+            }
+        }
+
+        dispatchIfValid(fetchWeatherStarted);
 
         return fetch(apiUrl).then((response) => {
             if (response.status !== 200) {
@@ -26,12 +35,12 @@ export const fetchWeather = (cityCode) => {
             }
 
             response.json().then((responseJson) => {
-                dispatch(fetchWeatherSuccess(responseJson.weatherinfo));
+                dispatchIfValid(fetchWeatherSuccess(responseJson.weatherinfo));
             }).catch((error) => {
-                dispatch(fetchWeatherFailure(error));
+                dispatchIfValid(fetchWeatherFailure(error));
             })
         }).catch((error) => {
-            dispatch(fetchWeatherFailure(error));
+            dispatchIfValid(fetchWeatherFailure(error));
         });
     };
 };
