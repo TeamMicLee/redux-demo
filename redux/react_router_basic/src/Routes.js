340ae38e357e0c1432f38d2d997ac4172ff34dd3
litao <TeamMicLee@foxmail.com>
2018-01-17 15:35:13 +0800

    增加一个计数器

--- redux/react_router_basic/src/Routes.js
+++ redux/react_router_basic/src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
+import {combineReducers} from 'redux';
 
 import {syncHistoryWithStore} from 'react-router-redux';
 
 import App from './pages/App.js';
-import Home from './pages/Home.js';
-import About from './pages/About.js';
-import NotFound from './pages/NotFound.js';
+// import Home from './pages/Home.js';
+// import Counter from './pages/CounterPage.js';
+// import About from './pages/About.js';
+// import NotFound from './pages/NotFound.js';
 import store from './Store.js';
 
 const createElement = (Component, props) => {
@@ -18,15 +20,51 @@ const createElement = (Component, props) => {
     );
 };
 
+const getHomePage = (nextState, callback) => {
+    require.ensure([], function(require) {
+        callback(null, require('./pages/Home.js').default);
+    }, 'home');
+};
+
+const getCounterPage = (nextState, callback) => {
+    require.ensure([], function(require) {
+        const { page, reducer, initialState, stateKey } = require('./pages/CounterPage.js');
+
+        const state = store.getState();
+        store.reset(combineReducers({
+            ...store._reducers,
+            counter: reducer
+        }), {
+            ...state,
+            [stateKey]: initialState
+        });
+        callback(null, page);
+    }, 'counter');
+};
+
+const getAboutPage = (nextState, callback) => {
+    require.ensure([], function(require) {
+        callback(null, require('./pages/About.js').default);
+    }, 'about');
+};
+
+const getNotFoundPage = (nextState, callback) => {
+    require.ensure([], function(require) {
+        callback(null, require('./pages/NotFound.js').default);
+    }, '404');
+};
+
+
 const history = syncHistoryWithStore(browserHistory, store);
 
 const Routes = () => (
     <Router history={history} createElement={createElement}>
         <Route path="/" component={App}>
-            <IndexRoute component={Home} />
-            <Route path="home" component={Home} />
-            <Route path="about" component={About} />
-            <Route path="*" component={NotFound} />
+            <IndexRoute getComponent={getHomePage} />
+            <Route path="home" getComponent={getHomePage} />
+            <Route path="counter" getComponent={getCounterPage} />
+            <Route path="about" getComponent={getAboutPage} />
+            <Route path="*" getComponent={getNotFoundPage} />
         </Route>
     </Router>
 );
